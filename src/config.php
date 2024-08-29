39dc6f73516bc1f3089645e243d298bdacaa1e26
Bryan Josue Sandí Quirós <brysq@estudiantec.cr>
2024-08-28 18:44:36 -0600

    [ADD] add config.php to gitignore

--- src/config.php
+++ src/config.php
@@ -5,6 +5,6 @@ $db_host = "delta";
 
 return [
     'dsn' => "mysql:host={$host};dbname={$db_host};charset=utf8",
-    'username' => 'admin',
-    'password' => 'BSQ_DB_admin2024!',
+    'username' => '',
+    'password' => '',
 ];
