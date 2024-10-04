c5d3f214562c3b9b7b046fb65dad731c27d55b9f
Sebastián Fernández <sebasr.ferval@estudiantec.cr>
2024-10-03 23:54:38 -0600

    [ADD] Result Map Style

--- frontend/forms/src/components/Results/Map/ResultMap.jsx
+++ frontend/forms/src/components/Results/Map/ResultMap.jsx
@@ -8,16 +8,10 @@ import customMarker from '../../helper/CustomMarker';
 
 import API_URL from '../../../config';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './ResultMap.css';
 
-
-const container = {
-  // rounded corners, light gray background, box shadow
-  borderRadius: '5px',
-  backgroundColor: '#f4f4f4',
-  boxShadow: '1px 1px 5px rgba(0, 0, 0, 0.1)',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'start',
+const resultmapStyle = {
+  height: '67vh',
 }
 
 
@@ -35,50 +29,51 @@ export const ResultMap = () => {
 
 
   return (
-    <div>
-      <h2>{data.form.name}</h2>
-      <p>{data.form.description}</p>
-      <div>
-        <h3>Descarga de los resultados</h3>
-        <a href={`${API_URL}/forms/results/csv/${id}`} download>
-          Descargar CSV
-        </a>
+    <div className='resultsmap'>
+      <div className="resultmap-info">
+          <h1>{data.form.name}</h1>
+          <p>{data.form.description}</p>
+          <a href={`${API_URL}/forms/results/csv/${id}`} download>
+            Descargar CSV
+          </a>
       </div>
-      <MapContainer center={[9.9725447, -84.1963422]} zoom={9} style={{ height: '100vh' }} >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {
-          data.results.map(({ submission_id, position, fields }) => {      
-            console.log(position);      
-            return (
-              <Marker key={submission_id} position={position} icon={customMarker}>
-                <Popup className='request-popup'>
-                {
-                  <div>
-                    <div>
-                      Creado: {data.form.created_at}
-                    </div>
-                    <div>
-                      Ubicacion: {JSON.stringify(position)}
-                    </div>
-                    {fields.map(({ field_id, field_name, answer }) => (
-                      <div key={field_id} style={container}>
-                        <h3>{field_name}</h3>
-                        <div>
-                          <p>{answer}</p>
+      <div className="resultmap-container">
+          <MapContainer center={[9.9725447, -84.1963422]} zoom={9} style={ resultmapStyle } >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {
+              data.results.map(({ submission_id, position, fields }) => {      
+                console.log(position);      
+                return (
+                  <Marker key={submission_id} position={position} icon={customMarker}>
+                    <Popup className='request-popup'>
+                    {
+                      <div className = "answers-container">
+                        <div className = "creation-date">
+                          Fecha de la respuesta: {data.form.created_at}
                         </div>
+                        <div>
+                          {/*Ubicacion: {JSON.stringify(position)}*/}
+                        </div>
+                        {fields.map(({ field_id, field_name, answer }) => (
+                          <div key={field_id} className="results-container">
+                            <h3>{field_name}</h3>
+                            <div>
+                              <p style={{ 'margin-top': 1, 'margin-bottom': 1} }>{answer}</p>
+                            </div>
+                          </div>
+                        ))}
                       </div>
-                    ))}
-                  </div>
-                }
-                </Popup>
-              </Marker>
-            );
-          })
-        }
-      </MapContainer>
+                    }
+                    </Popup>
+                  </Marker>
+                );
+              })
+            }
+          </MapContainer>
+      </div>  
     </div>
   );
 };
