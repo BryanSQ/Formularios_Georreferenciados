60fa7fc9541bebc1b41b5fb390a81ab5d78ec557
jostinrl8 <86120943+jostinrl8@users.noreply.github.com>
2024-11-19 18:54:35 -0600

    config.js change

--- frontend/forms/src/config.js
+++ frontend/forms/src/config.js
@@ -1,5 +1,5 @@
 const API_URL = process.env.NODE_ENV === 'production' 
-    ? 'http://150.136.56.106/api'  // IP pública de producción
+    ? 'http://129.159.93.14/api'  // IP pública de producción
     : 'http://localhost/api';  // Localhost en desarrollo
 
 export default API_URL;
