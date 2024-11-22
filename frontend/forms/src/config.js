a79af27ab865dc5118f8bd562ac6a511385b04a8
BryanSQ <sandibry04@gmail.com>
2024-11-22 15:43:39 -0600

    Experimental build

--- frontend/forms/src/config.js
+++ frontend/forms/src/config.js
@@ -1,5 +1,5 @@
 const API_URL = process.env.NODE_ENV === 'production' 
-    ? 'http://129.159.93.14/api'  // IP pública de producción
+    ? 'YOUR_API_URL_HERE/api'  // IP pública de producción
     : 'http://localhost/api';  // Localhost en desarrollo
 
 export default API_URL;
